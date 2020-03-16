6f027b0f5477fad35791a073c3c4bc1e23dc08d0
pasignature <pasignature@gmail.com>
2020-03-16 21:03:26 +0000

    added printf prototypes to the header file

--- holberton.h
+++ holberton.h
@@ -1,5 +1,29 @@
 #ifndef HOLBERTON_HEADER
 #define HOLBERTON_HEADER
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
+int pr_char(va_list ap);
+
+int pr_str(va_list ap);
+
+int pr_int(va_list ap);
+
+int _printf(const char *format, ...);
+
+int pr_rot13(va_list ap);
+
+int pr_rev(va_list ap);
+
+int convert(va_list ap, int base);
+
+int convertU(va_list ap, int base);
+
+int non_prting(va_list ap);
+
+int pr_ptr(va_list ap);
 
 #endif
