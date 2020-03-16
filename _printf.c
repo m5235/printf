900eec3ab70908cfc335356b7c352300ab78c0d3
m5235 <m.ladhari@outlook.com>
2020-03-17 00:28:33 +0100

    task

--- _printf.c
+++ _printf.c
@@ -1,48 +1,58 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <stdarg.h>
-/**
- * _printf - print a given string and its arguments
- * @format: string
- * Return: number of characters printed
- */
+#include "holberton.h"	
 
-int _printf(const char *format, ...)
-{
-	va_list list;
-	unsigned int i = 0;
-	int (*print)(va_list);
-	int count = 0;
+/**	
+ * _printf - prints a formatted string from input	
+ * @format: string to be printed	
+ *	
+ * Return: length of string	
+ */	
+int _printf(const char *format, ...)	
+{	
+	va_list ap;	
+	int j = 0, k = 0;	
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		else if (format[i] == '%' && format[i + 1] != '\0')
-		{
-			i++;
-
-				if (print == 0)
-				{
-					_putchar('%');
-					_putchar(format[i]);
-					count += 2;
-				}
-				else
-					count += print(list);
-			}
-		}
-		else
-			return (-1);
-		i++;
-	}
-	va_end(list);
-	return (count);
-}
+	va_start(ap, format);	
+	while (format[j] != '\0')	
+	{	
+		if (format[j] == '%')	
+		{	
+			j++;	
+			while (format[j] == ' ')	
+				j++;	
+			if (format[j] == 's')	
+				k += pr_str(ap);	
+			else if (format[j] == 'c')	
+				k += pr_char(ap);	
+			else if (format[j] == 'd' || format[j] == 'i' ||	
+				 format[j] == 'u')	
+				k += pr_int(ap);	
+			else if (format[j] == '%')	
+				write(1, "%", 1);	
+			else if (format[j] == 'R')	
+				k += pr_rot13(ap);	
+			else if (format[j] == 'r')	
+				k += pr_rev(ap);	
+			else if (format[j] == 'x')	
+				k += convert(ap, 16);	
+			else if (format[j] == 'X')	
+				k += convertU(ap, 16);	
+			else if (format[j] == 'o')	
+				k += convert(ap, 8);	
+			else if (format[j] == 'b')	
+				k += convert(ap, 2);	
+			else if (format[j] == 'S')	
+				k += non_prting(ap);	
+			else if (format[j] == 'p')	
+				k += pr_ptr(ap);	
+			else	
+				write(1, &format[j - 1], 2);	
+		} else	
+		{	
+			k++;	
+			write(1, &format[j], 1);	
+		}	
+		j++;	
+	}	
+	va_end(ap);	
+	return (k);	
+} 
