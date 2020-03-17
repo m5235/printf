8985fd7395e2572bcacf9ac195fd491c57d95019
pasignature <pasignature@gmail.com>
2020-03-17 19:25:38 +0000

    modified all files

--- _printf.c
+++ _printf.c
@@ -23,8 +23,27 @@ int _printf(const char *format, ...)
 				k += pr_str(ap);
 			else if (format[j] == 'c')
 				k += pr_char(ap);
+			else if (format[j] == 'd' || format[j] == 'i' ||
+				 format[j] == 'u')
+				k += pr_int(ap);
 			else if (format[j] == '%')
 				write(1, "%", 1);
+			else if (format[j] == 'R')
+				k += pr_rot13(ap);
+			else if (format[j] == 'r')
+				k += pr_rev(ap);
+			else if (format[j] == 'x')
+				k += convert(ap, 16);
+			else if (format[j] == 'X')
+				k += convertU(ap, 16);
+			else if (format[j] == 'o')
+				k += convert(ap, 8);
+			else if (format[j] == 'b')
+				k += convert(ap, 2);
+			else if (format[j] == 'S')
+				k += non_prting(ap);
+			else if (format[j] == 'p')
+				k += pr_ptr(ap);
 			else
 				write(1, &format[j - 1], 2);
 		} else
